76c37a7b935174bdfde01bc99c4afbac649d68ea
ISHAAN KOTHIDAR <25415242+ikothidar@users.noreply.github.com>
2026-07-08 12:49:53 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hash_map = {}
+        for char in s:
+            hash_map[char] = hash_map[char] + 1 if char in hash_map else 1
+
+        for char in t:
+            if char not in hash_map or hash_map[char] - 1 < 0:
+                return False
+            else:
+                hash_map[char] = hash_map[char] - 1
+
+        for i in hash_map:
+            if hash_map[i] != 0:
+                return False
+
+        return True
